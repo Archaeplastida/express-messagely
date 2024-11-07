903dc7bd2f2d3a612aea5b9b1fa88d0061d3cb59
Saif <saifisami2005@gmail.com>
2024-11-07 18:54:25 -0500

    all static async function to bring basic info on all users.

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { 
+  static async register({ username, password, first_name, last_name, phone }) {
     let hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
     const result = await db.query(`INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at) VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp) RETURNING username, password, first_name, last_name`, [username, hashedPassword, first_name, last_name, phone]);
     return result.rows[0];
@@ -22,7 +22,7 @@ class User {
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { 
+  static async authenticate(username, password) {
     const result = await db.query(`SELECT password FROM users WHERE username = $1`, [username]);
     let user = result.rows[0];
     return user && await bcrypt.compare(password, user.password);
@@ -32,13 +32,16 @@ class User {
 
   static async updateLoginTimestamp(username) {
     const result = await db.query(`UPDATE users SET last_login_at = current_timestamp WHERE username = $1 RETURNING username`, [username]);
-    if(!result.rows[0]) throw new ExpressError(`No such user: ${username}`, 404);
-   }
+    if (!result.rows[0]) throw new ExpressError(`No such user: ${username}`, 404);
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() {
+    const result = await db.query(`SELECT username, first_name, last_name, phone FROM users ORDER by username`);
+    return result.rows;
+   }
 
   /** Get: get user by username
    *
