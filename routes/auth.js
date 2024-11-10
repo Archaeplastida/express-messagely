85156913f1cf0a91bad8d6b3b35c8145837efe30
Saif <saifisami2005@gmail.com>
2024-11-09 20:54:04 -0500

    Removed extra comments.

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,3 @@
-/** POST /login - login: {username, password} => {token}
- *
- * Make sure to update their last-login!
- *
- **/
-
-
-/** POST /register - register user: registers, logs in, and returns token.
- *
- * {username, password, first_name, last_name, phone} => {token}.
- *
- *  Make sure to update their last-login!
- */
-
-
 const jwt = require("jsonwebtoken"), Router = require("express").Router, router = new Router(), User = require("../models/user"), { SECRET_KEY } = require("../config"), ExpressError = require("../expressError");
 
 //logs user in if correct username and password is given, then returning a token.
